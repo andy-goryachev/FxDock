e366e4fa049f20779d8e653887776cdd9940ce34
andy goryachev <andy@goryachev.com>
2024-04-06 10:51:25 -0700

    2024

--- src/goryachev/fxdock/FxDockPane.java
+++ src/goryachev/fxdock/FxDockPane.java
@@ -1,4 +1,4 @@
-// Copyright © 2016-2023 Andy Goryachev <andy@goryachev.com>
+// Copyright © 2016-2024 Andy Goryachev <andy@goryachev.com>
 package goryachev.fxdock;
 import goryachev.fx.FX;
 import goryachev.fx.FxAction;
--- src/goryachev/fxdock/internal/FxDockBorderPane.java
+++ src/goryachev/fxdock/internal/FxDockBorderPane.java
@@ -1,4 +1,4 @@
-// Copyright © 2016-2023 Andy Goryachev <andy@goryachev.com>
+// Copyright © 2016-2024 Andy Goryachev <andy@goryachev.com>
 package goryachev.fxdock.internal;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.ReadOnlyProperty;
